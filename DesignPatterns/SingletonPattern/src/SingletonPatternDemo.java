66ac6a1d3b3db44bc66eeaaf8448c4a2b6e81ee1
MarkSun <meetmark.sun@gmail.com>
2019-07-30 22:18:32 -0700

    Singleton Demo

---
+++ DesignPatterns/SingletonPattern/src/SingletonPatternDemo.java
@@ -0,0 +1,13 @@
+public class SingletonPatternDemo {
+
+    public static void main(String[] args) {
+
+        //illegal construct
+        //Compile Time Error: The constructor SingleObject() is not visible
+        //SingleObject object = new SingleObject();
+
+        SingleObject object = SingleObject.getInstance();
+
+        object.showMessage();
+    }
+}
